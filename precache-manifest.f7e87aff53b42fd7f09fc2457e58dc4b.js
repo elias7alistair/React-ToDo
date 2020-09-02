4344d09ace9918fb7d80da0cbae7bfdee43a6a16
elias7alistair <falistair7@gmail.com>
2020-09-02 15:39:15 +0530

    Updates

---
+++ precache-manifest.f7e87aff53b42fd7f09fc2457e58dc4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea96de94046f6c2b2bf7b66c08314a11",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "9dbde5f7f4bd4a8e2118",
+    "url": "/React-ToDo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0c44beeae6aed05a07b8",
+    "url": "/React-ToDo/static/css/main.8f2bba90.chunk.css"
+  },
+  {
+    "revision": "9dbde5f7f4bd4a8e2118",
+    "url": "/React-ToDo/static/js/2.3125e6af.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-ToDo/static/js/2.3125e6af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c44beeae6aed05a07b8",
+    "url": "/React-ToDo/static/js/main.2d8753af.chunk.js"
+  },
+  {
+    "revision": "035243a428f2d5393a36",
+    "url": "/React-ToDo/static/js/runtime-main.265918f0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-ToDo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "90d8040b311460ba6e34e58e1db8b24c",
+    "url": "/React-ToDo/static/media/holder.90d8040b.jpg"
+  }
+]);
